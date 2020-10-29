df9cc197c029073d230e7c76a17ed6fd75dc3106
Peti <49650243+Petiazediter@users.noreply.github.com>
2020-10-29 22:31:42 +0100

    Empty recylcer added to note fragment

--- app/src/main/java/com/codecool/practiceapp/MainActivity.kt
+++ app/src/main/java/com/codecool/practiceapp/MainActivity.kt
@@ -6,12 +6,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
             .add(R.id.main_frame_layout, NoteFragment())
             .commit()
-    }
 
+    }
 }
